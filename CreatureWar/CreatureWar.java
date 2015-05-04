a4dfff6110a9e10d4d1c2f961750f190b02efec5
Aristolchia <G00231538@stu.raritanval.edu>
2015-05-04 11:23:55 -0400

    Fixed Array Out of bounds Exception
    fixed an issue within the CreatureWar class method "battle"
    the while statement had an OR operator and needed an AND

--- CreatureWar/CreatureWar.java
+++ CreatureWar/CreatureWar.java
@@ -11,7 +11,7 @@ public class CreatureWar
     private ArrayList<Creature> sideOne;
     private ArrayList<Creature> sideTwo;
     public Random r = new Random();
-    int i = r.nextInt(10)+5;
+    int i = r.nextInt(100)+50;
     /**
      * Constructor for objects of class CreatureWar
      */
@@ -50,29 +50,29 @@ public class CreatureWar
    public void battle()
    {
        int j = 0;
-       int k = 0;
-       while(j<i || k<i)
+       int h = 0;
+       while(j < i && h < i)
        {
-           sideOne.get(j).takeDamage(sideTwo.get(k).attack());
-           System.out.println("Unit " + k + "From Army Two Deals " + sideTwo.get(k).attack() + "Damage to Unit " + j + "in Army One");
-           sideTwo.get(k).takeDamage(sideOne.get(j).attack());
-           System.out.println("Unit " + j + "From Army One Deals " + sideTwo.get(j).attack() + "Damage to Unit " + k + "in Army Two");
+           sideOne.get(j).takeDamage(sideTwo.get(h).attack());
+           System.out.println("Unit " + h + " From Army Two Deals " + sideTwo.get(h).attack() + " Damage to Unit " + j + " in Army One");
+           sideTwo.get(h).takeDamage(sideOne.get(j).attack());
+           System.out.println("Unit " + j + " From Army One Deals " + sideTwo.get(j).attack() + " Damage to Unit " + h + " in Army Two");
            if(sideOne.get(j).isAlive() != true)
            {   
-               System.out.println("Unit " + j + "From Army One Has Died!");
+               System.out.println("Unit " + j + " From Army One Has Died!");
                j++;
            }
-           if(sideTwo.get(k).isAlive() != true)
+           if(sideTwo.get(h).isAlive() != true)
            {   
-               System.out.println("Unit " + k + "From Army Two Has Died!");
-               k++;
+               System.out.println("Unit " + h + " From Army Two Has Died!");
+               h++;
            }
         }
-        if(j<k)
+        if(j<h)
         {
             System.out.println("Army Two Wins!");
         }
-        else if(j>k)
+        else if(j>h)
         {
             System.out.println("Army One Wins!");
         }
--- CreatureWar/War.java
+++ CreatureWar/War.java
@@ -26,6 +26,6 @@ public class War
     {
         CreatureWar cW = new CreatureWar();
         cW.createArmy();
-        
+        cW.battle();
     }
 }
